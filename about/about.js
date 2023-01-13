f27e692d86735077888f19ec598f42d993c21751
henewi <hpdunn2002@gmail.com>
2023-01-13 10:18:12 +0000

    button placement

--- about/about.js
+++ about/about.js
@@ -7,3 +7,18 @@ const fadeOut = () => {
 }
 
 window.addEventListener('load', fadeOut);
+
+// UNHIDE //
+
+function showHideDiv(ele) {
+    var srcElement = document.getElementById(ele);
+    if (srcElement != null) {
+        if (srcElement.style.display == "block") {
+            srcElement.style.display = 'none';
+        }
+        else {
+            srcElement.style.display = 'block';
+        }
+        return false;
+    }
+}
