7a538ee3f8efee4f8c76fd91a7e30b978d5279e7
henewi <hpdunn2002@gmail.com>
2023-01-15 10:12:44 +0000

    email form added

--- contact/contact.js
+++ contact/contact.js
@@ -14,5 +14,32 @@ $(document).ready(function () {
     $('.submit').click(function (event) {
         event.preventDefault()
         console.log('CLICKED')
+
+        var email = $('.email').val()
+        var subject = $('.subject').val()
+        var message = $('.message').val()
+        var statusElm = $('.status')
+        statusElm.empty()
+
+        if(email.length > 5 && email.includes('@') && email.includes('.')) {
+            statusElm.append('<div>email is valid</div>')
+        }
+        else {
+            statusElm.append('<div>email is not valid</div>')
+        }
+
+        if(subject.length >= 2) {
+            statusElm.append('<div>Subject is valid</div>')
+        }
+        else {
+            statusElm.append('<div>Subject is not valid</div>')
+        }
+
+        if(message.length > 0) {
+            statusElm.append('<div>Message is valid</div>')
+        }
+        else {
+            statusElm.append('<div>Message is empty</div>')
+        }
     })
 })
